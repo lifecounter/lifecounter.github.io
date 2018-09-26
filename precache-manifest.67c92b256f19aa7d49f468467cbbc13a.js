33627a16813be4930c573b86cba659dbcee2d99a
TechnoTom <t.ludwig@markenstuermer.de>
2018-09-26 14:47:28 +0200

    deploy

---
+++ precache-manifest.67c92b256f19aa7d49f468467cbbc13a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/js/chunk-vendors.59645627.js"
+  },
+  {
+    "revision": "8036fa5a53eb9b68241f",
+    "url": "/js/app.43839a6a.js"
+  },
+  {
+    "revision": "23354afce5a855f8291a",
+    "url": "/js/about.d4a2497d.js"
+  },
+  {
+    "revision": "73d2cff96e79ca67533c88581cc5a6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8036fa5a53eb9b68241f",
+    "url": "/css/app.5500103a.css"
+  }
+];
